b62e91bca9fa49235def773c7cdb7b0e894bc749
nesquikpro <nnsquik@outlook.com>
2026-03-12 10:18:22 +0300

    add filepath args and format option

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,6 +5,15 @@ import picocli.CommandLine;
 @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
+    @CommandLine.Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @CommandLine.Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @CommandLine.Option(names = {"-f", "--format"}, paramLabel = "format",
+            description = "output format [default: stylish]", defaultValue = "stylish")
+    private String format;
 
     public static void main(String[] args) {
         new CommandLine(new App()).execute(args);
